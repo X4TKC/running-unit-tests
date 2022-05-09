893884df36976597a83b7127cdb2416f1495d6a1
X4TKC <tovilaskevin@gmail.com>
2022-05-08 23:58:50 -0400

    first commit

---
+++ src/test.java
@@ -0,0 +1,33 @@
+package net.petrikainulainen.junit5;
+
+public class test{
+     void justAnExample() {
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+    }
+    void justAnExample2() {
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+    }
+    void justAnExample3() {
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+    }
+
+
+}
---
+++ src/test/java/net/petrikainulainen/junit5/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+    void justAnExample() {
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+    }
+    void justAnExample2() {
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+    }
+    void justAnExample3() {
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+        System.out.println("This test method should be run");
+    }
+}
